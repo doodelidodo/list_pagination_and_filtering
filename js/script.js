187473e1ec116c7f60ce5d158fa45d30083a2526
Dominic Meyer <dominic.meyer@me.com>
2020-02-16 13:27:08 +0100

    Extra Credit

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ FSJS project 2 - List Filter and Pagination
 
 let studentList = document.querySelectorAll('.student-item');
 const numberOfItemsPerPage = 10;
+const pageContent = document.querySelector('.page');
 
 
 /*** 
@@ -59,7 +60,6 @@ const showPage = (list, page) => {
 
 const appendPageLinks = (list, numberPerPage) => {
    let pages = Math.ceil(list.length / numberPerPage);
-   const body = document.querySelector('body');
 
    const div = document.createElement('div');
    div.className = "pagination";
@@ -68,9 +68,9 @@ const appendPageLinks = (list, numberPerPage) => {
    div.appendChild(ul);
 
    appendListElements(ul, pages);
-   paginationClickListener(ul);
+   paginationClickListener(list, ul);
 
-   body.appendChild(div);
+   pageContent.appendChild(div);
 };
 
 
@@ -102,16 +102,15 @@ const appendListElements = (ul, pages) => {
  the click activate the showPage function to change the shown list elements
  ***/
 
-const paginationClickListener = (ul) => {
+const paginationClickListener = (list, ul) => {
    const allLinks = ul.childNodes;
-
    for(let i = 0; i < allLinks.length; i++) {
       allLinks[i].firstChild.addEventListener('click', (e) => {
-         let clickedLink = e.target;
-         let pageNr = e.target.textContent;
-         removeActiveClass(allLinks);
-         clickedLink.className = "active";
-         showPage(studentList, pageNr)
+      let clickedLink = e.target;
+      let pageNr = e.target.textContent;
+      removeActiveClass(allLinks);
+      clickedLink.className = "active";
+      showPage(list, pageNr)
       });
    }
 };
@@ -127,6 +126,87 @@ const removeActiveClass = (list) => {
    }
 };
 
+
+/***
+ function appendSearchBar
+ append the search bar to the page
+
+ template:
+ <div class="student-search">
+   <input placeholder="Search for students...">
+ </div>
+ ***/
+
+const appendSearchBar = () => {
+  const div = document.createElement('div');
+  div.className = "student-search";
+
+  const input = document.createElement('input');
+  input.setAttribute('placeholder', "Search for students...");
+  div.appendChild(input);
+
+  const headerText = document.querySelector("h2");
+  headerText.parentNode.insertBefore(div, headerText.nextSibling);
+
+  const divNoResult = createNoResultDiv();
+
+  input.addEventListener('keyup', (e) => {
+     divNoResult.style.display = "none";
+     let name = e.target.value;
+     let pagination = document.querySelector('.pagination');
+     let newStudentList = [];
+     for(let i = 0; i < studentList.length; i++) {
+        let studentName = studentList[i].querySelector('h3').textContent;
+        studentList[i].style.display = "none";
+        if(studentName.includes(name)) {
+           studentList[i].style.display = "block";
+           newStudentList.push(studentList[i]);
+        }
+        pagination.remove();
+     }
+
+     showPage(newStudentList, 1);
+     appendPageLinks(newStudentList, numberOfItemsPerPage);
+     showNoResult(newStudentList, divNoResult);
+  });
+
+};
+
+/***
+ function createNoResultDiv
+ param1: list
+ param2: element
+
+ Creates a div with the message no Results and set the div to display none
+ ***/
+const createNoResultDiv = () => {
+   const divNoResult = document.createElement('div');
+   divNoResult.className = "no-result";
+   divNoResult.style.display = "none";
+   divNoResult.textContent = "No result found!";
+
+   const ulList = document.querySelector("ul");
+   ulList.parentNode.insertBefore(divNoResult, ulList.nextSibling);
+   return divNoResult;
+};
+
+
+/***
+ function showNoResult
+ param1: list
+ param2: element
+
+ if the list is empty, the element gets display block.
+ ***/
+
+const showNoResult = (list, element) => {
+    if(!list.length) {
+       element.style.display = "block";
+    }
+};
+
+
+
 /***
 sets the view to the first items on window.onload
  and creates the pagination Links
@@ -134,4 +214,5 @@ sets the view to the first items on window.onload
 window.onload = () => {
    showPage(studentList, 1);
    appendPageLinks(studentList, numberOfItemsPerPage);
+   appendSearchBar();
 };
